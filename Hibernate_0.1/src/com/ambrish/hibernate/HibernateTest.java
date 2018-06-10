66db2e9283b617d9184d5e973107ef5c2ce5da0e
Ambrish Pandey <ambrishpandey2010@gmail.com>
2018-06-10 17:25:19 +0530

    first commit

---
+++ Hibernate_0.1/src/com/ambrish/hibernate/HibernateTest.java
@@ -0,0 +1,25 @@
+package com.ambrish.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ambrish.dto.UserDetails;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UserDetails user = new UserDetails();
+		user.setUserId(108);
+		user.setUserName("Ambrish");
+		user.setCity("Bangalore");
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.getTransaction().commit();
+	}
+
+}
